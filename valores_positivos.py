df27b034f4a0021370458b1298f71aae34bde01c
adeniasfilho <adeniasfilho.n1@gmail.com>
2022-04-19 12:19:14 -0300

    Programa lê 6 valores positivos e/ou negativos:imprime positivos
    Signed-off-by: adeniasfilho <adeniasfilho.n1@gmail.com>

---
+++ valores_positivos.py
@@ -0,0 +1,8 @@
+#TODO: Complete os espaços em branco com uma solução possível para o problema.
+counter = 0
+for _ in range(6):
+    number = float(input())
+    if number > 0:
+      counter +=1
+       
+print("{} valores positivos".format(counter))
